8d3ee3fddf5e7a471a45fc8acb2ff05764e8a78f
Kunal <kunalcode01@gmail.com>
2026-07-12 18:43:00 +0000

    updates

--- programs/novaforge/src/instructions/mod.rs
+++ programs/novaforge/src/instructions/mod.rs
@@ -1,9 +1,9 @@
 pub mod initialize_planet;
-pub mod colonize_planet;
+pub mod colonize_planet;//
 pub mod uncolonize_planet;
 pub mod claim_resources;
 pub mod upgrade_military;
-pub mod upgrade_planet;
+pub mod upgrade_planet;//
 pub mod check_threat; //
 pub mod list_planet; //
 pub mod repair_planet;
